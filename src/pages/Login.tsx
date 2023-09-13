6bb0097f5286b2d6d0f3a8f3feb9f603a9fafb18
confidential-nt <youyoy689@gmail.com>
2023-09-13 15:41:58 +0900

    [feat] 로그인 페이지, 회원가입 페이지, 메인 페이지 랄우팅 설정

---
+++ src/pages/Login.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Login() {
+  return <div>여기는 로그인페이지</div>;
+}
---
+++ src/pages/Register.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Register() {
+  return <div>여기는 회원가입 페이지</div>;
+}
