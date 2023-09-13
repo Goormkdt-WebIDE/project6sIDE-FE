41f6520067fa9f90e40e610959845156df15ac34
confidential-nt <youyoy689@gmail.com>
2023-09-13 15:43:18 +0900

    로그인 페이지, 회원가입 페이지, 메인 페이지 라우팅 설정 (#2)
    * [chore] react-router-dom 의존성 추가
    
    * [feat] 로그인 페이지, 회원가입 페이지, 메인 페이지 랄우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Layout from "./layout/Layout";
+import Main from "./pages/Main";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
-    <>
-      <h1 className="bg-red-500">Vite + React</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index path="/" element={<Main />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
+    </Routes>
   );
 }
 
