53d9de36560a9f874ab7aa9b6d5610bcc715d15f
confidential-nt <youyoy689@gmail.com>
2023-09-25 17:02:19 +0900

    [test] PasswordInput 테스트 작성

---
+++ src/components/test/PasswordInput.test.tsx
@@ -0,0 +1,48 @@
+import "@testing-library/jest-dom";
+import PasswordInput, { RegisterType } from "../PasswordInput";
+import { FieldError } from "react-hook-form";
+import { render, screen } from "@testing-library/react";
+
+describe("PasswordInput", () => {
+  const register = jest.fn();
+
+  afterEach(() => {
+    register.mockReset();
+  });
+
+  it("스냅샷 테스트", () => {
+    const { asFragment } = renderPasswordInput();
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("placeholder prop을 전달하면 그것을 올바르게 render 해야한다", () => {
+    const placeholder = "Password를 입력하세요";
+    renderPasswordInput(placeholder);
+    expect(screen.getByPlaceholderText(placeholder)).toBeInTheDocument();
+  });
+
+  it("required 에러가 발생하면 밑에 에러가 표시되어야한다", () => {
+    renderPasswordInput("", { type: "required" } as FieldError);
+    expect(screen.getByRole("alert")).toBeInTheDocument();
+  });
+
+  it("minLength 에러가 발생하면 밑에 에러가 표시되어야한다", () => {
+    renderPasswordInput("", { type: "minLength" } as FieldError);
+    expect(screen.getByRole("alert")).toBeInTheDocument();
+  });
+
+  function renderPasswordInput(
+    placeholder?: string,
+    error?: FieldError,
+    register_type?: RegisterType
+  ) {
+    return render(
+      <PasswordInput
+        register={register}
+        errors={{ password: error }}
+        placeholder={placeholder}
+        register_type={register_type}
+      />
+    );
+  }
+});
