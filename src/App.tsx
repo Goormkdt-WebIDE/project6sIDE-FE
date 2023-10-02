ce1a501f165839e75e242e2dc9077700b145fb67
confidential-nt <youyoy689@gmail.com>
2023-10-02 20:05:56 +0900

    [feat] 유저 로그인 시 유저 정보 저장하는 기능 구현

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,21 @@ import Login from "./pages/Login";
 import PasswordReset from "./pages/PasswordReset";
 import IDE from "./pages/IDE";
 import Main from "./pages/Main";
+import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/password-reset" element={<PasswordReset />} />
-        <Route path="/workspace" element={<IDE />} />
-      </Route>
-    </Routes>
+    <AuthContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/password-reset" element={<PasswordReset />} />
+          <Route path="/workspace" element={<IDE />} />
+        </Route>
+      </Routes>
+    </AuthContextProvider>
   );
 }
 
