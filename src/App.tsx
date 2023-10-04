1e2d2698b9e972cbc6f3e91f647fd451fb0634d1
confidential-nt <youyoy689@gmail.com>
2023-10-04 11:17:17 +0900

    [refactor] AuthContextProvider 위치 수정

--- src/App.tsx
+++ src/App.tsx
@@ -7,21 +7,18 @@ import Login from "./pages/Login";
 import PasswordReset from "./pages/PasswordReset";
 import IDE from "./pages/IDE";
 import Main from "./pages/Main";
-import { AuthContextProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <AuthContextProvider>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/password-reset" element={<PasswordReset />} />
-          <Route path="/workspace" element={<IDE />} />
-        </Route>
-      </Routes>
-    </AuthContextProvider>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index path="/" element={<Main />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/password-reset" element={<PasswordReset />} />
+        <Route path="/workspace/:projectname" element={<IDE />} />
+      </Route>
+    </Routes>
   );
 }
 
