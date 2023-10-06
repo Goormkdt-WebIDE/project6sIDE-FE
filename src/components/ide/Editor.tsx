824d9456556a3f96fbc5345d59ddddead34410af
confidential-nt <youyoy689@gmail.com>
2023-10-06 10:39:57 +0900

    [feat] 파일 확장자에 따라 에디터 모드 변경하는 기능 구현

--- src/components/ide/Editor.tsx
+++ src/components/ide/Editor.tsx
@@ -1,30 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AceEditor from "react-ace";
 
-import { config } from "ace-builds";
+import * as ace from "ace-builds";
 import "ace-builds/src-noconflict/theme-monokai";
 import "ace-builds/src-noconflict/ext-language_tools";
+import modelist from "ace-builds/src-noconflict/ext-modelist";
 import { Code } from "./TreeView";
 
-config.set("basePath", "/node_modules/ace-builds/src-min-noconflict");
+ace.config.set("basePath", "/node_modules/ace-builds/src-min-noconflict");
 
 type Props = {
   file: Code | null;
 };
 
 export default function Editor({ file }: Props) {
+  const [value, setValue] = useState<string>("");
+
+  useEffect(() => {
+    setValue(file && file.text ? file.text : "");
+  }, [file]);
+
   return (
     <AceEditor
-      mode={file && file.extension ? file.extension : "java"}
+      mode={file ? getModeByFileExtension(file.name) : "java"}
       theme="monokai"
-      onChange={() => {}}
+      onChange={(value) => {
+        setValue(value);
+      }}
       name="UNIQUE_ID_OF_DIV"
       editorProps={{ $blockScrolling: true }}
-      value={file?.text}
+      value={value}
       style={{
         flexBasis: "80%",
         height: "100%",
       }}
     />
   );
+}
+
+function getModeByFileExtension(path: string) {
+  return modelist.getModeForPath(path).name;
 }
