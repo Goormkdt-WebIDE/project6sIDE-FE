b58504777f2a97da46298e3abb72fdc71c045c6c
confidential-nt <youyoy689@gmail.com>
2023-10-10 23:12:44 +0900

    [fix] IDE 풀 스크린에서 로그아웃 버튼 - 테마 토글 버튼 안보이는 문제 수정

--- src/layout/IDELayout.tsx
+++ src/layout/IDELayout.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import TopBtnContainer from "../components/TopBtnContainer";
 
 export default function IDELayout() {
   return (
     <div className="relative">
+      <TopBtnContainer className="hidden md:flex md:!-top-1" />
       <main className="h-screen overflow-hidden pb-4">
         <Outlet />
         <ToastContainer
